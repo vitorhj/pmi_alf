f564b5d8aa8a126f6359b548601740d7c2a02696
vitorhj <90215531+vitorhj@users.noreply.github.com>
2021-09-17 16:49:04 -0300

    Update app_analise_de_risco.py

--- app_analise_de_risco.py
+++ app_analise_de_risco.py
@@ -32,7 +32,7 @@ if texto_aprova != "":
     
     #incricao_aprova = " ".join(texto_aprova_split[index_aprova3 + 1:index_aprova3 + 2])
     inscricao_aprova = re.findall(r'\d\d\d.\d\d\d.\d\d.\d\d\d\d.\d\d\d\d.\d\d\d', texto_aprova)
-    st.text(inscricao_aprova)
+    st.text(inscricao_aprova[0])
 
     trecho_aprova = " ".join(texto_aprova_split[index_aprova1:index_aprova2])
 
