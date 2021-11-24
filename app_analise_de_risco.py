887e761888664638e1a003b8ca69327a9fcb74b6
vitorhj <90215531+vitorhj@users.noreply.github.com>
2021-11-23 22:27:04 -0300

    Update app_analise_de_risco.py

--- app_analise_de_risco.py
+++ app_analise_de_risco.py
@@ -176,7 +176,7 @@ try:
         endereço_split = re.sub(' +', ' ',logradouro_aprova).split(' ')
         logradouro_google = "+".join(endereço_split)
         st.markdown('MAPS: '+str('https://www.google.com/maps/place/')+logradouro_google+str(',+')+str(numero_aprova)+str('+,+Itaja%C3%AD+-+SC'))
-        st.markdown('VIABILIDADE: '+str('https://geoitajai.github.io/geo/consultaalvara.html#')+inscricao_aprova[0:3]+inscricao_aprova[4:6]+inscricao_aprova[12:15])
+        st.markdown('VIABILIDADE: '+str('https://geoitajai.github.io/geo/consultaalvara.html#')+inscricao_aprova[0:3]+inscricao_aprova[4:7]+inscricao_aprova[11:15])
         
         #Printa a verificação do cnpj
         st.text('____________________________________________________________________________________________________________')
