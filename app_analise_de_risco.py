27bbee050a748858ec9171e029577754f5f9be94
vitorhj <90215531+vitorhj@users.noreply.github.com>
2022-02-21 14:47:24 -0300

    Update app_analise_de_risco.py

--- app_analise_de_risco.py
+++ app_analise_de_risco.py
@@ -254,7 +254,7 @@ try:
         tabela_cnaes = pd.read_csv('./dados/grau_risco_maio_2021.xlsx - Página2.csv', sep=',')
         tabela_cnaes
         
-        nova_tabela=tabela_cnaes.merge(cnaes_cnpj,how='left',left_on='codigo', right_on='0')
+        #nova_tabela=tabela_cnaes.merge(cnaes_cnpj,how='left',left_on='codigo', right_on='0')
         
         #Verificação armas de fogo
         
