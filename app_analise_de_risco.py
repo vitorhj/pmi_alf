31663850ecf308d45765214827ff2af3b22d97e0
vitorhj <90215531+vitorhj@users.noreply.github.com>
2022-05-02 16:52:35 -0300

    Update app_analise_de_risco.py

--- app_analise_de_risco.py
+++ app_analise_de_risco.py
@@ -220,7 +220,7 @@ if st.sidebar.button('Limpar'):
 ##ESTRUTURA PAGINA VERIFICAÇÃO DE PROCESSOS
 
 st.title('PMI - ANÁLISE DE RISCO')
-if texto_aprova or somente_cnpj == '' or ' ':
+if texto_aprova == '':
      st.subheader(str('Links úteis'))
      st.markdown('CNPJ: '+str('https://servicos.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp'))
      st.markdown('REGIN: '+str('http://regin.jucesc.sc.gov.br/regin.externo/CON_ViabilidadeSelecaoExternoV4.aspx?'))
