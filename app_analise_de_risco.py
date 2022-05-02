b578f5724b4e36adc128ae431e6445a0bb93fc9d
vitorhj <90215531+vitorhj@users.noreply.github.com>
2022-05-02 14:33:00 -0300

    Update app_analise_de_risco.py

--- app_analise_de_risco.py
+++ app_analise_de_risco.py
@@ -13,6 +13,18 @@ from PIL import Image
 
 st.set_page_config(layout="wide")
 
+st.set_page_config(
+     page_title="ALF - Pref. de Itajaí",
+     page_icon="▫️",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
+
 ## EXTRAI INFORMAÇÕES DO APROVA ##
 
 # Input box do aprova
