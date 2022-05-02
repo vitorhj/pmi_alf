9667de58335bb4effaf1e0baac17af36c1a18472
vitorhj <90215531+vitorhj@users.noreply.github.com>
2022-05-02 17:50:42 -0300

    Update app_analise_de_risco.py

--- app_analise_de_risco.py
+++ app_analise_de_risco.py
@@ -228,14 +228,14 @@ if texto_aprova == '':
      st.markdown('GEO: '+str('https://geoitajai.github.io/geo/plantacadastral.html'))
      st.markdown('<<< Copie e cole as informações na barra lateral esquerda.')
 
-#try:
-     #if somente_cnpj != "":
-          #st.subheader(str('Dados do cartão CNPJ'))
-          #st.text('CNPJ: '+numero_cnpj2)
-          #st.markdown('RAZÃO SOCIAL: '+razao_social_cnpj2)
-          #st.markdown('ENDEREÇO: '+logradouro_cnpj2+', '+numeropredial_cnpj2+', '+bairro_cnpj2+' '+complemento_cnpj2)         
-#except:
-  #pass
+try:
+     if somente_cnpj != "":
+          st.subheader(str('Dados do cartão CNPJ'))
+          st.text('CNPJ: '+numero_cnpj2)
+          st.markdown('RAZÃO SOCIAL: '+razao_social_cnpj2)
+          st.markdown('ENDEREÇO: '+logradouro_cnpj2+', '+numeropredial_cnpj2+', '+bairro_cnpj2+' '+complemento_cnpj2)         
+except:
+  pass
      
 
 try:
