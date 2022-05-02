7ca6f4938315a68e39bca7477400bfe1a1daa1cb
vitorhj <90215531+vitorhj@users.noreply.github.com>
2022-05-02 17:55:17 -0300

    Update app_analise_de_risco.py

--- app_analise_de_risco.py
+++ app_analise_de_risco.py
@@ -231,7 +231,7 @@ if (texto_aprova or somente_cnpj) == '':
 try:
      if somente_cnpj != "":
           st.subheader(str('Dados do cartão CNPJ'))
-          st.text('CNPJ: '+numero_cnpj2)
+          #st.text('CNPJ: '+numero_cnpj2)
           st.markdown('RAZÃO SOCIAL: '+razao_social_cnpj2)
           st.markdown('ENDEREÇO: '+logradouro_cnpj2+', '+numeropredial_cnpj2+', '+bairro_cnpj2+' '+complemento_cnpj2)         
 except:
